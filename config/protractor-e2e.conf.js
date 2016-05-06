582f303f4d61c357c718a0b10224066a9490ad12
Alex Seriy <alex@pagewire.com>
2016-05-05 23:00:17 -0400

    Modified Travis CI config to run e2e from Grunt.

---
+++ config/protractor-e2e.conf.js
@@ -0,0 +1,8 @@
+exports.config = {
+	framework: 'jasmine',
+	seleniumAddress: 'http://localhost:4444/wd/hub',
+	specs: ['test/e2e/scenarios.js'],
+	capabilities: {
+		browserName: 'phantomjs'
+	}
+}
